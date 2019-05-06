e3d1474ce4003a0effa7ca2f41540c03c077be75
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-05-06 12:55:54 -0500

    nav bug fixed

--- app-files/client/src/components/layout/Page1.js
+++ app-files/client/src/components/layout/Page1.js
@@ -3,12 +3,15 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 //import { Link } from "react-router-dom";
+import Navbar from '../navbar/Navbar';
 
 class Page1 extends Component {
   render() {
     const { user } = this.props.auth;
     
     return (
+      <div>
+      <Navbar />  
       <div className="container valign-wrapper">
         <div className="row">
           <div className="col s12 center-align">
@@ -17,6 +20,7 @@ class Page1 extends Component {
             </h1>
           </div>
         </div>
+      </div>
       </div>
     );
   }

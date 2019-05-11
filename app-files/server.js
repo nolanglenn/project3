d231db4cdf062dfdb5b1be2d242b7c7f0e3394b3
theAndes <age.esquivel@gmail.com>
2019-05-11 13:36:23 -0500

    Does not need to be merged to master

--- app-files/server.js
+++ app-files/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
-const passport = require("passport");
+const passport = require('passport');
 
-const users = require("./routes/api/users");
+const users = require('./routes/api/users');
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
@@ -14,22 +14,20 @@ app.use(
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 // Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/LocalOpenHouse'
+);
 
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 // Routes
-app.use("/api/users", users);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`The server is up and running on port ${port} !`));
+app.listen(port, () =>
+  console.log(`The server is up and running on port ${port} !`)
+);

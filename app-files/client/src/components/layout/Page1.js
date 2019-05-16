7202cbe365cd4895462ef82fbaa0a0331ca33c75
nolanglenn <nolangglenn@gmail.com>
2019-05-15 21:02:19 -0500

    Form layout complete

--- app-files/client/src/components/layout/Page1.js
+++ app-files/client/src/components/layout/Page1.js
@@ -21,31 +21,42 @@ class Page1 extends Component {
               <div className="row">
                 <form className="col s12">
                   <div className="row">
+                    <div className="input-field col s12">
+                      <input placeholder="" id="job-title" type="text" className="validate"></input>
+                      <label for="job-title">What do you need help with?</label>
+                    </div>
+                  </div>
+                  <div className="row">
                     <div className="input-field col s6">
-                      <input placeholder="Placeholder" id="first_name" type="text" className="validate"></input>
-                      <label for="first_name">First Name</label>
+                      <input placeholder="0.00" id="compensation" type="text" className="validate"></input>
+                      <label for="compensation">Compensation</label>
                     </div>
                     <div className="input-field col s6">
-                      <input id="last_name" type="text" className="validate"></input>
-                      <label for="last_name">Last Name</label>
+                    <select>
+                      <option value="" disabled selected>Choose the job type</option>
+                      <option value="1">Open house</option>
+                      <option value="2">Showing</option>
+                      <option value="3">Title work</option>
+                    </select>
+                    <label>Job Type</label>
+                  </div>
+                  </div>
+                  <div className="row">
+                    <div className="input-field col s12">
+                      <input id="address" type="text" className="validate"></input>
+                      <label for="address">Address</label>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="input-field col s6">
+                      <input type="text" className="datepicker"></input>
+                      <label for="datepicker">Date</label>
                     </div>
                   </div>
                   <div className="row">
                     <div className="input-field col s12">
-                      <input disabled value="I am not editable" id="disabled" type="text" className="validate"></input>
-                      <label for="disabled">Disabled</label>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input id="password" type="password" className="validate"></input>
-                      <label for="password">Password</label>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="input-field col s12">
-                      <input id="email" type="email" className="validate"></input>
-                      <label for="email">Email</label>
+                      <input id="notes" type="text" className="validate"></input>
+                      <label for="notes">Notes</label>
                     </div>
                   </div>
                 </form>

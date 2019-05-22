1165114b871018c49871b230cfb79da6c329af7c
theAndes <age.esquivel@gmail.com>
2019-05-22 18:16:53 -0500

    fixing the error on job page and fix class -> className

--- client/src/components/layout/Page2.js
+++ client/src/components/layout/Page2.js
@@ -1,69 +1,74 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { logoutUser } from "../../actions/authActions";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 //import { Link } from "react-router-dom";
 import Navbar from '../navbar/Navbar';
 import { List } from '../List';
-import { ListItem} from '../List';
+import { ListItem } from '../List';
 
 class Page2 extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       errors: '',
       jobs: null
     };
-
   }
 
   componentDidMount() {
     //TODO: make api call to grab selected jobs, pass those objects in below...
     setTimeout(() => {
       this.setState({
-        jobs: [{
-          // originUser: this.props.auth.user.id,
-          jobID: 1,
-          jobTitle: 'Temp Job 1- Open House',
-          compensation: '$50',
-          jobType: 'Open House'
-        }, {
-          jobID: 2,
-          jobTitle: 'Temp Job 2 - Open House',
-          compensation: '$50',
-          jobType: 'Open House'
-        }, {
-          jobID: 3,
-          jobTitle: 'Temp Job 3 - Open House',
-          compensation: '$50',
-          jobType: 'Open House'
-        }, {
-          jobID: 4,
-          jobTitle: 'Temp Job 4- Open House',
-          compensation: '$50',
-          jobType: 'Open House'
-        }]
-      })
-    }, 1000)
+        jobs: [
+          {
+            // originUser: this.props.auth.user.id,
+            jobID: 1,
+            jobTitle: 'Temp Job 1- Open House',
+            compensation: '$50',
+            jobType: 'Open House'
+          },
+          {
+            jobID: 2,
+            jobTitle: 'Temp Job 2 - Open House',
+            compensation: '$50',
+            jobType: 'Open House'
+          },
+          {
+            jobID: 3,
+            jobTitle: 'Temp Job 3 - Open House',
+            compensation: '$50',
+            jobType: 'Open House'
+          },
+          {
+            jobID: 4,
+            jobTitle: 'Temp Job 4- Open House',
+            compensation: '$50',
+            jobType: 'Open House'
+          }
+        ]
+      });
+    }, 1000);
   }
-
 
   render() {
     const { user } = this.props.auth;
     if (!this.state.jobs) {
-      return <p>Loading...</p>
+      return <p>Loading...</p>;
     }
     return (
       <div>
         <Navbar />
 
-        <div className="container" style={{ width: '80%', margin: '1rem auto' }}>
-          <div className="row" >
+        <div
+          className="container"
+          style={{ width: '80%', margin: '1rem auto' }}
+        >
+          <div className="row">
             <div className="col s12 center-align">
               <h4>
                 <b>Available Jobs:</b>
-                <hr style={{ width: '80%' }}></hr>
+                <hr style={{ width: '80%' }} />
               </h4>
             </div>
           </div>
@@ -76,14 +81,20 @@ class Page2 extends Component {
                     <div className="row">
                       <div className="col-6 col-md-4">
                         <a href={jobs.jobID} target="_blank">
-                          <strong style={{fontSize: '1.5rem'}}>
+                          <strong style={{ fontSize: '1.5rem' }}>
                             {jobs.jobTitle}
                           </strong>
                         </a>
-                        <a class="waves-effect waves-light btn-small" style={{marginLeft: '20px'}}>Detail</a>
+                        <a
+                          className="waves-effect waves-light btn-small"
+                          style={{ marginLeft: '20px' }}
+                        >
+                          Detail
+                        </a>
                       </div>
                     </div>
-                  </ListItem>))}
+                  </ListItem>
+                ))}
               </List>
             </div>
           </div>

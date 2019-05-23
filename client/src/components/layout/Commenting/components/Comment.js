bae9db883b2b471ea976c1f5b6ee840279c8f8f4
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-05-22 21:11:29 -0500

    finished user commenting

--- client/src/components/layout/Commenting/components/Comment.js
+++ client/src/components/layout/Commenting/components/Comment.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Avatar from 'react-avatar';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../../../actions/authActions';
  
 class Comment extends Component {
   render() {
+
+    const { user } = this.props.auth;
+
     return (
         <div style={{textAlign: 'center'}} className='row z-depth-0'>
           <div className='col s2'>
-            <Avatar style={{margin: '10px auto 10px auto'}} name="Wim Mostmans" src='' size="60" round={true}/>
+            <Avatar style={{margin: '10px auto 10px auto'}} name={this.props.auth.user.name} src='' size="60" round={true}/>
           </div>
           <div className='col s10'>
             <p style={{textAlign: 'left'}}>
@@ -20,5 +26,16 @@ class Comment extends Component {
     );
   }
 }
- 
-export default Comment;
+
+Comment.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Comment);
